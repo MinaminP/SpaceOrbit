b78f7027bc47ab028120cbd5238c4c8afaa87c09
MinaminP <amienghostlike@gmail.com>
2021-10-09 20:21:56 +0700

    add build

--- SpaceOrbit/assets/Script/testScript.js
+++ SpaceOrbit/assets/Script/testScript.js
@@ -29,18 +29,28 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
+        this.node.parent.on('mousedown', this.change_rotation, this);
         //var action = cc.repeatForever(cc.rotateBy(5,360));
        // this.node.runAction(action);
     },
 
+    change_rotation:function(event){
+       if(this.isFlipped == true){
+           this.isFlipped = false;
+       } else if (this.isFlipped == false){
+           this.isFlipped = true;
+       }
+    },
     start () {
 
     },
 
     update (dt) {
         if(this.isFlipped == true){
+            this.node.scale.x = -1;
             this.planet.angle += (dt * this.speed)
         } else if(this.isFlipped == false){
+            this.node.scale.x = 1;
             this.planet.angle -= (dt * this.speed);
         }
     },
